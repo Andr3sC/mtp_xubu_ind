72949366a10dd39855d90c616aef22916d4d516d
Andres C <Andres C>
2014-02-09 14:25:04 +0100

    Desdoblamos las clases

--- mtp_app.py
+++ mtp_app.py
@@ -2,62 +2,58 @@
 import sys
 from gi.repository import Gtk
 import logging
+from RunLoggingHandlerTextBuffer import RunLoggingHandlerTextBuffer
 
-class RunLoggingHandler(logging.Handler):
-     def __init__(self, textbuffer):
-         logging.Handler.__init__(self)
-         self.textbuffer = textbuffer
 
-     def utf8conv(self,x):
-         try:
-             return unicode(x,'utf8')
-         except:
-             return x
-
-     def write(self, record):
-        self.textbuffer.insert_at_cursor(record)#+'\n') 
-        
-     def emit(self, record):
-         self.textbuffer.insert_at_cursor(record.getMessage()+'\n') 
-         	
 class mtp_app:
 
     def on_window_destroy(self, widget, data=None):
         Gtk.main_quit()
-     
+
     def on_switch1_activate(self, widget, data=None, extra=None):
-        self._log.debug("Estado ["+str(self.switcher.get_active())+"]")
-        
- 
+        switcher_state = not self.switcher.get_active()
+        self._log.debug("Estado [" + str(switcher_state) + "]")
+        if (self.connection_state and not switcher_state):
+            self._log.debug("Deactivating")
+        elif (not self.connection_state and switcher_state):
+            self._log.debug("Activating")
+        else:
+            self._log.debug("No change")
+        # last but not least
+        self.connection_state = switcher_state
+
     def __init__(self):
         builder = Gtk.Builder()
-        builder.add_from_file("glade_files/main_window.xml") 
-        
+        builder.add_from_file("glade_files/main_window.xml")
         self.window = builder.get_object("window1")
         self.switcher = builder.get_object("switch1")
         self.text_view = builder.get_object("textview1")
         self.tb = self.text_view.get_buffer()
-        self.tlh = RunLoggingHandler(self.tb)
+        self.tlh = RunLoggingHandlerTextBuffer(self.tb)
         logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                    datefmt='%Y-%m-%d %H:%M',
-                    stream=self.tlh
-                    )
-                    #,filename='/tmp/myapp.log',
-                    #filemode='w')
+                            format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                            datefmt='%Y-%m-%d %H:%M',
+                            stream=self.tlh
+                            )
+        #,filename='/tmp/myapp.log',
+        # filemode='w')
         self._log = logging.getLogger(__name__)
-        #self._log.addHandler(self.tlh) 
-        #self._log.setLevel(logging.DEBUG)
-        #self.tb.insert_at_cursor("hola")
+        # self._log.addHandler(self.tlh)
+        # self._log.setLevel(logging.DEBUG)
+        # self.tb.insert_at_cursor("hola")
         self._log.debug("Arrancando3")
-        builder.connect_signals(self)       
-        #self.switcher.connect("button-press-event",self.on_switch1_activate,None)
-        self.switcher.set_active(True)
+        builder.connect_signals(self)
+        # self.switcher.connect("button-press-event" ,
+        # self.on_switch1_activate,None)
+        self.connection_state = (False)
+        self.switcher.set_active(self.connection_state)
         self._log.debug("Arrancando4")
-    
+        switcher_state = not self.switcher.get_active()
+        self._log.debug("Estado [" + str(switcher_state) + "]")
+
+
 if __name__ == "__main__":
     editor = mtp_app()
     editor.window.show()
     Gtk.main()
     sys.exit(1)
-
